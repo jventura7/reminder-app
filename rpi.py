99b47bff7d355a90f4966bd47a7d2d74e9b19d91
noahsanzone <60456771+noahsanzone@users.noreply.github.com>
2022-05-07 14:51:33 -0400

    initial upload

---
+++ rpi.py
@@ -0,0 +1,74 @@
+#Author: Noah Sanzone
+
+import pika
+
+# incomming message: username (routing key), title, message, time reminder is set for
+
+
+dict1 = {"username": "user1",
+         "title": "meds",
+         "message": "take your meds",
+         "time": "6:00"}
+
+# dict2 = {"username": "user2",
+#          "title": "walk",
+#          "message": "go for a walk",
+#          "time": "12:00"}
+#
+# dict3 = {"username": "user3",
+#          "title": "work",
+#          "message": "do your work",
+#          "time": "18:00"}
+
+# need to see if user already exists in the queue
+
+# get the username
+username = dict1.get("username")
+
+raspIP = "172.29.107.22"
+
+# Establish RabbitMQ connection
+credentials = pika.PlainCredentials('admin', 'password')
+parameters = pika.ConnectionParameters(raspIP,
+                                           5672,
+                                           '/',
+                                           credentials)
+
+connection = pika.BlockingConnection(parameters)
+channel = connection.channel()
+
+action = "p"
+warehouse = "FinalProject"
+collection = username
+dict = dict1
+
+if action == "p":
+    channel.basic_publish(exchange=warehouse,
+                          routing_key=collection,
+                          body=dict)
+
+
+
+
+
+
+# guardian submits a post request
+# dependent submit a get request
+
+
+
+
+
+
+
+
+#
+# connection = pika.BlockingConnection(parameters)
+# channel = connection.channel()
+
+# tutorial on youtube techwithtim for flask
+# John's IP: 172.29.45.136
+
+# make a file with reminders and get it working using a priority queue
+# make a dictionary that acts as reminder
+
